0243941b4a0f152d57e5a181177d89affc9de63b
ラフィ <70844466+Rafi-Darussalam@users.noreply.github.com>
2026-04-01 19:04:36 +0700

    Add more image for card

--- src/Components/Home/HomeCard.tsx
+++ src/Components/Home/HomeCard.tsx
@@ -11,11 +11,12 @@ export default function HomeCard({ title, subtitle, href, linkTitle, src, alt, c
   className?: string,
 }) {
   return(
-    <div className={`flex-1 h-78 relative bg-linear-to-b from-transparent via-transparent to-[#000000] rounded-md ${className}`}>
+    <div className={`flex-1 h-78 relative bg-black rounded-2xl overflow-hidden ${className}`}>
       <div className="absolute w-full h-full">
-        <ImageAutoSize src={src} alt={alt} className="object-contain" />
+        <ImageAutoSize src={src} alt={alt} className="object-cover" />
       </div>
-      <div className="w-full h-full flex flex-col justify-end text-white pt-25 px-6 py-6">
+      <div className="w-full h-full absolute bg-linear-to-b from-transparent via-transparent to-[#000000]" />
+      <div className="relative w-full h-full flex flex-col justify-end text-white pt-25 px-6 py-6">
         <h3 className="text-[1.9rem] font-bold">{title}</h3>
         <p>{subtitle}</p>
         <Link href={href} className="text-accent">
