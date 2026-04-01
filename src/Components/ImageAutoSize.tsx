523a99da6293a7ed9a5ff893f1feb8dd9ed4de4a
ラフィ <70844466+Rafi-Darussalam@users.noreply.github.com>
2026-04-01 19:07:03 +0700

    Add sizes prop to image component

--- src/Components/ImageAutoSize.tsx
+++ src/Components/ImageAutoSize.tsx
@@ -1,13 +1,14 @@
 import Image from "next/image"
 
-export default function ImageAutoSize({ src, alt, className }: {
+export default function ImageAutoSize({ src, alt, className, sizes }: {
   src: string,
   alt: string,
-  className?: string
+  className?: string,
+  sizes?: string
 }) {
   return (
     <div className="w-full h-full relative">
-      <Image src={src} alt={alt} fill className={className} />
+      <Image src={src} alt={alt} fill className={className} sizes={sizes} />
     </div>
   )
 }
