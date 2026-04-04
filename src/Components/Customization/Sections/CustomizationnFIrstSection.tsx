a0e8b42a5bc48ba8949ed9a3ba398083cda6350e
ラフィ <70844466+Rafi-Darussalam@users.noreply.github.com>
2026-04-04 17:57:26 +0700

    Fix some layouting

--- src/Components/Customization/Sections/CustomizationnFIrstSection.tsx
+++ src/Components/Customization/Sections/CustomizationnFIrstSection.tsx
@@ -5,10 +5,10 @@ import { GiGearStickPattern } from "react-icons/gi";
 export default function CustomizationFirstSection() {
   return (
     <section className="bg-bg-primary">
-      <article className="flex flex-col md:flex-row gap-20 px-9 py-20">
+      <article className="flex flex-col md:flex-row gap-10 px-9 py-20">
         <div className="flex-2 flex-col">
           <h2 className="text-white text-[2rem] font-bold pb-2.5">THE PULSE OF THE NEW ERA</h2>
-          <p className="text-white pb-2.5">
+          <p className="text-white pb-2.5 text-[0.9rem]">
             The Revuelto is the first HPEV (High Performance Electrified Vehicle) hybrid super sports car. With its naturally aspirated V12 engine and three electric motors, it represents a paradigm shift in performance, sportiness and driving pleasure.
           </p>
           <div className="flex items-center gap-4 px-2.5 py-4 bg-bg-secondary text-white rounded-md mb-2.5">
@@ -16,8 +16,8 @@ export default function CustomizationFirstSection() {
               <FaBoltLightning />
             </div>
             <div className="flex flex-col">
-              <h3 className="text-white font-medium">TRIPLE ELECTRIC POWER</h3>
-              <p>Two motors on the front axle and one integrated into the gearbox provide full torque vectoring and AWD capability</p>
+              <h3 className="text-white font-medium text-[0.9rem]">TRIPLE ELECTRIC POWER</h3>
+              <p className="text-[0.8rem]">Two motors on the front axle and one integrated into the gearbox provide full torque vectoring and AWD capability</p>
             </div>
           </div>
           <div className="flex items-center gap-4 px-2.5 py-4 bg-bg-secondary text-white rounded-md">
@@ -25,8 +25,8 @@ export default function CustomizationFirstSection() {
               <GiGearStickPattern />
             </div>
             <div className="flex flex-col">
-              <h3 className="text-white font-medium">8-SPEED DUAL CLUTCH</h3>
-              <p>A new transverse gearbox architecture that is lighter and faster than ever before</p>
+              <h3 className="text-white font-medium text-[0.9rem]">8-SPEED DUAL CLUTCH</h3>
+              <p className="text-[0.8rem]">A new transverse gearbox architecture that is lighter and faster than ever before</p>
             </div>
           </div>
         </div>
