d70708f09c31685e256133f8890279b74b7b2193
ラフィ <70844466+Rafi-Darussalam@users.noreply.github.com>
2026-04-06 19:18:05 +0700

    Add and change img to Customization 2nd section

--- src/Components/Customization/Sections/CustomizationSecondSection.tsx
+++ src/Components/Customization/Sections/CustomizationSecondSection.tsx
@@ -7,18 +7,18 @@ export default function CustomizationSecondSection() {
       <div className="flex flex-col md:flex-row w-full md:h-130 gap-5 py-15">
         <div className="flex-3">
           <CustomizationArticleCard 
-            src="/Revuelto.png" title="MONOFUSELAGE" article="The chassis is a carbon-fiber structure, the first of its kind to feature a front structure in Forged Composites. It delivers 25% higher torsional stiffness than the Aventador."
+            src="/RevueltoCust.jpg" title="MONOFUSELAGE" article="The chassis is a carbon-fiber structure, the first of its kind to feature a front structure in Forged Composites. It delivers 25% higher torsional stiffness than the Aventador."
           />
         </div>
         <div className="flex-2 flex flex-col gap-5">
           <div className="flex-1">
             <CustomizationArticleCard 
-              src="/Revuelto.png" title="AERODYNAMIC" article="Active rear wing increases downforce by 66% in high-load conditions compared to its predecessor."
+              src="/RevueltoAD.png" title="AERODYNAMIC" article="Active rear wing increases downforce by 66% in high-load conditions compared to its predecessor."
             />
           </div>
           <div className="flex-1">
             <CustomizationArticleCard
-              src="/Revuelto.png" title="FORGED COMPOSITES" article="Patented technology allowing for complex shapes while maintaining extreme structural integrity and lightness."
+              src="/RevueltoFC.png" title="FORGED COMPOSITES" article="Patented technology allowing for complex shapes while maintaining extreme structural integrity and lightness."
             />
           </div>
         </div>
